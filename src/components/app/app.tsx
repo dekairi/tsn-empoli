2638aa64b9d663de759628e17668fc1e6f51cfe8
dekairi <foaxdev@yahoo.com>
2023-10-13 18:15:51 +0200

    -

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,5 @@
 import {GlobalStyle} from "./styles";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {HashRouter as Router, Route, Routes} from "react-router-dom";
 import React, {FC} from "react";
 import ScrollTop from "../ui/scroll-top/scroll-top";
 import {AppRoute} from "../../const";
